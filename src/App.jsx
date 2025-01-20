75245e0a75249b4b986458f38144e2268b1a6e09
teslo <teslo.knife@gmail.com>
2025-01-20 12:12:09 +0500

    Overwritten App.jsx without JSX

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
@@ -11,30 +11,54 @@ export const App = () => {
 	const [count, setCount] = useState(0);
 
 	// Декларативный стиль - описание структуры интерфейса
-	return (
-		<>
-			<div>
-				<a href="https://vite.dev" target="_blank">
-					<img src={viteLogo} className="logo" alt="Vite logo" />
-				</a>
-				<a href="https://react.dev" target="_blank">
-					<img src={reactLogo} className="logo react" alt="React logo" />
-				</a>
-			</div>
-			<h1>Vite + React</h1>
-			<div className="card">
-				<button onClick={() => setCount((count) => count + 1)}>
-					count is {count}
-				</button>
-				<p>
-					Edit <code>src/App.jsx</code> and save to test HMR
-				</p>
-			</div>
-			<p className="read-the-docs">
-				Click on the Vite and React logos to learn more
-			</p>
-			{/* Декларативный стиль: вывод текущего года */}
-			<p> Текущий год: {nowYear}</p>
-		</>
+	return React.createElement(
+		React.Fragment,
+		null,
+		// div с логотипами
+		React.createElement(
+			'a',
+			{ href: 'https://vite.dev', target: '_blank' },
+			React.createElement('img', {
+				src: viteLogo,
+				className: 'logo',
+				alt: 'Vite logo',
+			}),
+		),
+		React.createElement(
+			'a',
+			{ href: 'https://react.dev', target: '_blank' },
+			React.createElement('img', {
+				src: reactLogo,
+				className: 'logo react',
+				alt: 'React logo',
+			}),
+		),
+		// Заголовок
+		React.createElement('h1', null, 'Vite + React'),
+		// Карточка с кнопкой
+		React.createElement(
+			'div',
+			{ className: 'card' },
+			React.createElement(
+				'button',
+				{ onClick: () => setCount((count) => count + 1) },
+				`count is ${count}`,
+			),
+			React.createElement(
+				'p',
+				null,
+				'Edit ',
+				React.createElement('code', null, 'src/App.jsx'),
+				' and save to test HMR',
+			),
+		),
+		// Подсказка
+		React.createElement(
+			'p',
+			{ className: 'read-the-docs' },
+			'Click on the Vite and React logos to learn more',
+		),
+		// Текущий год
+		React.createElement('p', null, `Текущий год: ${nowYear}`),
 	);
 };
